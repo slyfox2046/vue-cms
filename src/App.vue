7bd98d170e4fcfb8def361245a93ee3788f0e3fd
slyfox <slyfox2046@gmail.com>
2018-12-04 11:14:59 +0800

    init porject

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <!--<mt-header fixed title="vue项目"></mt-header>-->
+    <mt-header fixed title="固定在顶部"></mt-header>
+
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+// import {Header} from 'mint-ui'
+// Vue.component(Header.name,Header)
+
+Vue.config.productionTip = false
+
+// 引入全部组件
+import Mint from 'mint-ui';
+Vue.use(Mint);
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
